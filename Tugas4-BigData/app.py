f67d2e1ddcff3229eacccd5c8ff1fa600ed4587a
Wahyu Ivan Satyagraha <32615673+wivans@users.noreply.github.com>
2019-05-02 00:22:06 +0700

    Update app.py

--- Tugas4-BigData/app.py
+++ Tugas4-BigData/app.py
@@ -15,7 +15,6 @@ from flask import Flask, request
 
 @main.route("/<int:user_id>/ratings/top/<int:count>", methods=["GET"])
 def top_ratings(user_id, count):
-    """"Untuk menampilkan sejumlah <count> rekomendasi film ke user <user_id>"""
     logger.debug("User %s TOP ratings requested", user_id)
     top_rated = recommendation_engine.get_top_ratings(user_id, count)
     return json.dumps(top_rated)
@@ -23,7 +22,6 @@ def top_ratings(user_id, count):
 
 @main.route("/movies/<int:movie_id>/recommend/<int:count>", methods=["GET"])
 def movie_recommending(movie_id, count):
-    """"Untuk menampilkan film <movie_id> terbaik direkomendasikan ke sejumlah <count> user"""
     logger.debug("MovieId %s TOP user recommending", movie_id)
     top_rated = recommendation_engine.get_top_movie_recommend(movie_id, count)
     return json.dumps(top_rated)
